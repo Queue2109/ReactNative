007f9f4903dfb3f8bb00620ab55eac4513201027
Queue2109 <hj2109@student.uni-lj.si>
2022-09-16 12:27:18 +0200

    Adding some style to a delete function

--- NPCourse/components/GoalItem.tsx
+++ NPCourse/components/GoalItem.tsx
@@ -3,24 +3,27 @@ import { StyleSheet, View, Text, Pressable } from "react-native";
 const GoalItem = (props: any) => {
   return (
     // bind preconfigures function for future execution
-    <Pressable onPress={props.onDeleteItem.bind(this, props.id)}>
-      <View style={styles.goalItem}>
-        <Text style={styles.goalText}>{props.text}</Text>
-      </View>
+    <View style={styles.goalItem}>
+       <Pressable android_ripple={{color: "#dddddd"}} onPress={props.onDeleteItem.bind(this, props.id)} style={({ pressed }) => pressed && styles.pressedItem} >
+       <Text style={styles.goalText}>{props.text}</Text>
     </Pressable>
+      </View>
   );
 };
-
+ 
 export default GoalItem;
 
 const styles = StyleSheet.create({
   goalItem: {
     margin: 8,
-    padding: 8,
     borderRadius: 6,
     backgroundColor: "purple",
   },
+  pressedItem: {
+    opacity: 0.5
+  },
   goalText: {
     color: "white",
+    padding: 8,
   },
 });
