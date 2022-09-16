aefe04abd698c3146682cff840306a15efc038c3
Queue2109 <hj2109@student.uni-lj.si>
2022-09-16 12:45:50 +0200

    Styling the modal overay

--- NPCourse/components/GoalInput.tsx
+++ NPCourse/components/GoalInput.tsx
@@ -1,9 +1,8 @@
 import { View, TextInput, Button, StyleSheet, Modal } from "react-native";
 import { useState } from "react";
 
-const GoalInput = (props:any) => {
-
-  const [enteredGoalText, setEnteredGoalText] = useState('');
+const GoalInput = (props: any) => {
+  const [enteredGoalText, setEnteredGoalText] = useState("");
 
   const goalInputHandler = (enteredText: string) => {
     setEnteredGoalText(enteredText);
@@ -11,40 +10,54 @@ const GoalInput = (props:any) => {
 
   const addGoalHandler = () => {
     props.onAddGoal(enteredGoalText);
-    setEnteredGoalText('');
-  }
-  
+    setEnteredGoalText("");
+  };
 
   return (
     <Modal visible={props.visible} animationType="slide">
-        <View style={styles.inputContainer}>
-            <TextInput
-            style={styles.textInput}
-            placeholder="Your course goal"
-            onChangeText={goalInputHandler}
-            value={enteredGoalText} />
-                            
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.textInput}
+          placeholder="Your course goal"
+          onChangeText={goalInputHandler}
+          value={enteredGoalText}
+        />
+        <View style={styles.buttonContainer}>
+          <View>
             <Button title="Add goal" onPress={addGoalHandler} />
+          </View>
+          <View style={styles.button}>
+            <Button title="Cancel"></Button>
+          </View>
         </View>
-    </Modal>);
+      </View>
+    </Modal>
+  );
 };
 
 const styles = StyleSheet.create({
   inputContainer: {
     flex: 1,
-    flexDirection: "row",
-    justifyContent: "space-between",
+    justifyContent: "center",
     alignItems: "center",
     marginBottom: 24,
+    padding: 16,
     borderBottomWidth: 1,
     borderBottomColor: "#cccccc",
   },
   textInput: {
     borderWidth: 1,
     borderColor: "#cccccc",
-    width: "80%",
-    marginRight: 8,
+    width: "100%",
     padding: 8,
+  },
+  buttonContainer: {
+    marginTop: 16,
+    flexDirection: "row",
+  },
+  button: {
+    width: 100,
+    marginHorizontal: 8,
   },
 });
 export default GoalInput;
