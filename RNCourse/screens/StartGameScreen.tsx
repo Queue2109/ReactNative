cb4633b8f0346fc477ad3da17e20342079f20309
Queue2109 <hj2109@student.uni-lj.si>
2022-09-17 12:23:37 +0200

    Styling for Android and iOS

--- RNCourse/screens/StartGameScreen.tsx
+++ RNCourse/screens/StartGameScreen.tsx
@@ -1,9 +1,9 @@
-import { TextInput, View } from "react-native";
+import { TextInput, View, StyleSheet } from "react-native";
 import PrimaryButton from "../components/PrimaryButton";
 
 const StartGameScreen = () => {
   return (
-    <View>
+    <View style={styles.inputContainer}>
       <TextInput />
       <PrimaryButton>Reset</PrimaryButton>
       <PrimaryButton>Confirm</PrimaryButton>
@@ -11,3 +11,21 @@ const StartGameScreen = () => {
   );
 };
 export default StartGameScreen;
+
+const styles = StyleSheet.create({
+    inputContainer: {
+        padding: 16,
+        marginTop: 100,
+        marginHorizontal: 24,
+        borderRadius: 8,
+        backgroundColor: '#72063c',
+        //adding a shadow... elevation has levels, the higher level, the more shadow
+        //it's an android only
+        elevation: 4,
+        //this works for iOS too
+        shadowColor: 'black',
+        shadowOffset: {width: 0, height:2},
+        shadowRadius: 6,
+        shadowOpacity: 0.25
+    },
+});
