b89c14df27872bb1595fd1d2f47646ccad44006f
Queue2109 <hj2109@student.uni-lj.si>
2022-09-17 12:30:08 +0200

    Styling the number input element

--- RNCourse/screens/StartGameScreen.tsx
+++ RNCourse/screens/StartGameScreen.tsx
@@ -4,7 +4,7 @@ import PrimaryButton from "../components/PrimaryButton";
 const StartGameScreen = () => {
   return (
     <View style={styles.inputContainer}>
-      <TextInput />
+      <TextInput style={styles.numberInput} maxLength={2} />
       <PrimaryButton>Reset</PrimaryButton>
       <PrimaryButton>Confirm</PrimaryButton>
     </View>
@@ -13,19 +13,30 @@ const StartGameScreen = () => {
 export default StartGameScreen;
 
 const styles = StyleSheet.create({
-    inputContainer: {
-        padding: 16,
-        marginTop: 100,
-        marginHorizontal: 24,
-        borderRadius: 8,
-        backgroundColor: '#72063c',
-        //adding a shadow... elevation has levels, the higher level, the more shadow
-        //it's an android only
-        elevation: 4,
-        //this works for iOS too
-        shadowColor: 'black',
-        shadowOffset: {width: 0, height:2},
-        shadowRadius: 6,
-        shadowOpacity: 0.25
-    },
+  inputContainer: {
+    padding: 16,
+    marginTop: 100,
+    marginHorizontal: 24,
+    borderRadius: 8,
+    backgroundColor: "#72063c",
+    //adding a shadow... elevation has levels, the higher level, the more shadow
+    //it's an android only
+    elevation: 4,
+    //this works for iOS too
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+    shadowOpacity: 0.25,
+  },
+  numberInput: {
+    height: 50,
+    width:50,
+    fontSize: 32,
+    borderBottomColor: '#ddb52f',
+    borderBottomWidth: 2,
+    color: '#ddb52f',
+    marginVertical: 8,
+    fontWeight: "bold",
+    textAlign: 'center'
+  } 
 });
