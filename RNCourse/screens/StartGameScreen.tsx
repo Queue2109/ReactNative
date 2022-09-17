5a1c3f70959ffb2473cf2dae2904465bddf83eef
Queue2109 <hj2109@student.uni-lj.si>
2022-09-17 12:32:53 +0200

    COnfiguring the textinput field

--- RNCourse/screens/StartGameScreen.tsx
+++ RNCourse/screens/StartGameScreen.tsx
@@ -4,7 +4,13 @@ import PrimaryButton from "../components/PrimaryButton";
 const StartGameScreen = () => {
   return (
     <View style={styles.inputContainer}>
-      <TextInput style={styles.numberInput} maxLength={2} />
+      <TextInput
+        style={styles.numberInput}
+        maxLength={2}
+        keyboardType="number-pad"
+        autoCapitalize="none"
+        autoCorrect={false}
+      />
       <PrimaryButton>Reset</PrimaryButton>
       <PrimaryButton>Confirm</PrimaryButton>
     </View>
@@ -30,13 +36,13 @@ const styles = StyleSheet.create({
   },
   numberInput: {
     height: 50,
-    width:50,
+    width: 50,
     fontSize: 32,
-    borderBottomColor: '#ddb52f',
+    borderBottomColor: "#ddb52f",
     borderBottomWidth: 2,
-    color: '#ddb52f',
+    color: "#ddb52f",
     marginVertical: 8,
     fontWeight: "bold",
-    textAlign: 'center'
-  } 
+    textAlign: "center",
+  },
 });
