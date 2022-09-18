acb196ff182b5ebbc302c65e68c75e36ae3003f4
Queue2109 <hj2109@student.uni-lj.si>
2022-09-18 20:52:42 +0200

    Starting work on the Game Screen

--- RNCourse/screens/GameScreen.tsx
+++ RNCourse/screens/GameScreen.tsx
@@ -1,7 +1,26 @@
-import { Text } from "react-native";
+import {StyleSheet, View, Text } from "react-native";
 
 const GameScreen = () => {
-  return <Text>Game screen</Text>;
+  return (
+    <View>
+      <Text>Opponent's guess</Text>
+      {/*GUESS(*/}
+      <View>
+        <Text>Higher or lower?</Text>
+        {/*   + -   */}
+      </View>
+      <View>
+        {/*LOG ROUNDS</View>*/}
+        </View>
+    </View>
+  );
 };
 
 export default GameScreen;
+
+const styles = StyleSheet.create({
+    screen: {
+        flex: 1, 
+        padding: 20
+    },
+});
