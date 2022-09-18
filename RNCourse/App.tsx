8a389b4400e9aec0157bff5a3ca6233502316472
Queue2109 <hj2109@student.uni-lj.si>
2022-09-18 18:12:03 +0200

    Adding a linear gradient

--- RNCourse/App.tsx
+++ RNCourse/App.tsx
@@ -1,13 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import StartGameScreen from './screens/StartGameScreen';
+import {LinearGradient} from 'expo-linear-gradient';
 
 export default function App() {
   return (
-    <StartGameScreen/>
+    //views only take as much space as they need to display its components
+    <LinearGradient style={styles.rootScreen} colors={["#4e0329", '#ddb52f']}>
+    <StartGameScreen/></LinearGradient>
   );
 }
 
 const styles = StyleSheet.create({
-  
+  rootScreen: {
+    flex: 1
+  }
 });
