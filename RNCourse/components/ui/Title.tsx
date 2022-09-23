a51b75ee4a0811ff146c9eb5b1d1d34ca82f0505
Queue2109 <hj2109@student.uni-lj.si>
2022-09-23 12:45:02 +0200

    Section 5 - Setting Dynamic widths

--- RNCourse/components/ui/Title.tsx
+++ RNCourse/components/ui/Title.tsx
@@ -16,5 +16,7 @@ const styles = StyleSheet.create({
     borderWidth: 2,
     borderColor: Colors.accent500,
     padding: 12,
+    // this is really useful - not all phones have a big screen and this maxWidth makes sure that the title component doesn't overflow
+    maxWidth: '80%'
   },
 });
