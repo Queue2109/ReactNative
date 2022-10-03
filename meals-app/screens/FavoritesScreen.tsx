f7a625e45e3c53b973a9dc7b17b66c99a2f161f0
Queue2109 <hj2109@student.uni-lj.si>
2022-10-03 05:05:38 +0200

    using Redux State in Components

--- meals-app/screens/FavoritesScreen.tsx
+++ meals-app/screens/FavoritesScreen.tsx
@@ -1,13 +1,17 @@
-import { useContext } from "react";
+//import { useContext } from "react";
 import MealsList from "../components/MealsList/MealsList";
-import { FavoritesContext } from "../store/context/favorites-content";
+//import { FavoritesContext } from "../store/context/favorites-content";
 import { MEALS } from "../data/dummy-data";
+import { useSelector } from "react-redux";
 import { View, Text, StyleSheet } from "react-native";
 
 const FavoritesScreen = () => {
-    const favoriteMealsContext = useContext(FavoritesContext);
+    //const favoriteMealsContext = useContext(FavoritesContext);
+    const favoriteMealIds = useSelector(
+        (state: any) => state.favoriteMeals.ids
+    );
     const favoriteMeals = MEALS.filter((meal: any) =>
-        favoriteMealsContext.ids.includes(meal.id)
+        favoriteMealIds.ids.includes(meal.id)
     );
 
     if (favoriteMeals.length === 0) {
